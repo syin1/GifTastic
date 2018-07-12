de2077b3a80435f49f18ef59ee67e68e95e6c982
syin <shaomingyinut@gmail.com>
2018-07-12 08:05:24 -0400

    set up apikey and basic ajax request

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,18 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  var apikey = 'fZ5dIQx9aTA23h20a4GGZGZvc7bf7hyj';
+  var searchTerm = 'ryan+gosling';
+
+  var queryURL =
+    'http://api.giphy.com/v1/gifs/search?q=' +
+    searchTerm +
+    '&api_key=' +
+    apikey +
+    '&limit=5';
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+  });
+});
