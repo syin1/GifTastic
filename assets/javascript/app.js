928905a99a67c246804e98a8e04148fbe00f2151
syin <shaomingyinut@gmail.com>
2018-07-12 20:37:18 -0400

    set up starter buttons based on topics, and finished its basic onclick functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,51 @@
 $(document).ready(function() {
-  var apikey = 'fZ5dIQx9aTA23h20a4GGZGZvc7bf7hyj';
-  var searchTerm = 'ryan+gosling';
+  var topics = [
+    'tennis',
+    'basketball',
+    'hockey',
+    'table tennis',
+    'golf',
+    'soccer',
+    'baseball',
+    'bowling',
+    'fencing',
+    'boxing'
+  ];
 
-  var queryURL =
-    'http://api.giphy.com/v1/gifs/search?q=' +
-    searchTerm +
-    '&api_key=' +
-    apikey +
-    '&limit=5';
+  var limit = 5;
 
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).then(function(response) {
-    console.log(response);
+  for (var i = 0; i < topics.length; i++) {
+    var onebutton = $("<button type='button', class='btn btn-success'>").text(
+      topics[i]
+    );
+    $('.buttons').append(onebutton);
+  }
+
+  $('button').on('click', function() {
+    var apikey = 'fZ5dIQx9aTA23h20a4GGZGZvc7bf7hyj';
+    var searchTerm = $(this).text();
+
+    var queryURL =
+      'http://api.giphy.com/v1/gifs/search?q=' +
+      searchTerm +
+      '&api_key=' +
+      apikey +
+      '&limit=' +
+      limit;
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(function(response) {
+      console.log(response);
+
+      for (var i = 0; i < limit; i++) {
+        oneimage = response.data[i].images.fixed_height.url;
+        imagediv = $('<img>');
+        imagediv.attr('src', oneimage);
+        imagediv.attr('alt', searchTerm + ' image');
+        $('.images').append(imagediv);
+      }
+    });
   });
 });
