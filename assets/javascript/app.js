5ea89750bdbd46f63f9093d11145e93210447ccd
syin <shaomingyinut@gmail.com>
2018-07-14 08:15:51 -0400

    buttons now load 10 gif images, added sidebar layout, updated page css margins

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     'boxing'
   ];
 
-  var limit = 5;
+  var limit = 10;
 
   for (var i = 0; i < topics.length; i++) {
     var onebutton = $("<button type='button', class='btn btn-success'>").text(
@@ -39,12 +39,24 @@ $(document).ready(function() {
     }).then(function(response) {
       console.log(response);
 
+      $('.images').text('');
+
       for (var i = 0; i < limit; i++) {
-        oneimage = response.data[i].images.fixed_height.url;
-        imagediv = $('<img>');
+        var oneimage = response.data[i].images.fixed_height.url;
+        var onerating = response.data[i].rating;
+
+        var container = $("<div class='imgcontainer'>");
+
+        var imagediv = $('<img>');
         imagediv.attr('src', oneimage);
         imagediv.attr('alt', searchTerm + ' image');
-        $('.images').append(imagediv);
+
+        var p = $('<p>').text('Rating: ' + onerating);
+
+        container.append(p);
+        container.append(imagediv);
+
+        $('.images').append(container);
       }
     });
   });
