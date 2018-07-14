d1df33147d01bbf7b02a73016c023f4e2b1ac2d9
syin <shaomingyinut@gmail.com>
2018-07-14 16:39:03 -0400

    implemented load more image feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,9 @@ $(document).ready(function() {
     'boxing'
   ];
 
+  var apikey = 'fZ5dIQx9aTA23h20a4GGZGZvc7bf7hyj';
   var limit = 10;
+  var currentTerm = '';
 
   function displayTopics() {
     $('.buttons').html('');
@@ -26,9 +28,34 @@ $(document).ready(function() {
 
   displayTopics();
 
+  function displayImages(response, searchTerm) {
+    for (var i = 0; i < limit; i++) {
+      var onegif = response.data[i].images.fixed_height.url;
+      var onestill = response.data[i].images.fixed_height_still.url;
+      var onerating = response.data[i].rating;
+
+      var container = $("<div class='imgcontainer'>");
+
+      var imagediv = $('<img>');
+      imagediv.attr('src', onestill);
+      imagediv.attr('data-still', onestill);
+      imagediv.attr('data-animate', onegif);
+      imagediv.attr('data-state', 'still');
+      imagediv.attr('alt', searchTerm + ' image');
+
+      var p = $('<p>').text('Rating: ' + onerating);
+
+      container.append(p);
+      container.append(imagediv);
+
+      $('.images').prepend(container);
+    }
+  }
+
   $(document.body).on('click', '.btn-category', function() {
-    var apikey = 'fZ5dIQx9aTA23h20a4GGZGZvc7bf7hyj';
+    limit = 10;
     var searchTerm = $(this).text();
+    currentTerm = searchTerm;
 
     var queryURL =
       'https://api.giphy.com/v1/gifs/search?q=' +
@@ -43,29 +70,7 @@ $(document).ready(function() {
       method: 'GET'
     }).then(function(response) {
       $('.images').text('');
-      console.log(response);
-
-      for (var i = 0; i < limit; i++) {
-        var onegif = response.data[i].images.fixed_height.url;
-        var onestill = response.data[i].images.fixed_height_still.url;
-        var onerating = response.data[i].rating;
-
-        var container = $("<div class='imgcontainer'>");
-
-        var imagediv = $('<img>');
-        imagediv.attr('src', onestill);
-        imagediv.attr('data-still', onestill);
-        imagediv.attr('data-animate', onegif);
-        imagediv.attr('data-state', 'still');
-        imagediv.attr('alt', searchTerm + ' image');
-
-        var p = $('<p>').text('Rating: ' + onerating);
-
-        container.append(p);
-        container.append(imagediv);
-
-        $('.images').append(container);
-      }
+      displayImages(response, searchTerm);
     });
   });
 
@@ -84,5 +89,25 @@ $(document).ready(function() {
       $(this).attr('src', $(this).attr('data-still'));
       $(this).attr('data-state', 'still');
     }
+  });
+
+  $('.loadmore').on('click', function() {
+    limit += 10;
+
+    var queryURL =
+      'https://api.giphy.com/v1/gifs/search?q=' +
+      currentTerm +
+      '&api_key=' +
+      apikey +
+      '&limit=' +
+      limit;
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(function(response) {
+      $('.images').text('');
+      displayImages(response, currentTerm);
+    });
   });
 });
